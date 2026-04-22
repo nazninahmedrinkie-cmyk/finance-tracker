27130581dc665b1a32eb77239454aea177c45b9a
Naznin Ahmed Rinkie <nazninahmedrinkie@gmail.com>
2026-04-22 13:34:52 +0530

    Delete frontend directory

--- frontend/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('App loads homepage', () => {
-  render(<App />);
-  
-  const element = screen.getByText(/FinTrack/i);
-  expect(element).toBeInTheDocument();
-});
